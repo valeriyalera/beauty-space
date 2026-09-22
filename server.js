d6253e568d845c058ad54b07e3d148647b0a928d
valeriyalera <valeriyalera2007@gmail.com>
2026-09-22 15:50:15 +0300

    Add dotenv and db initialization to server.js

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
-// server.js
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const db = require('./db');
 const productRoutes = require('./routes/productRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Чтобы сервер понимал данные из форм и JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Делаем папку public общедоступной (для картинок и стилей)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Настраиваем шаблонизатор EJS (View)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Подключаем маршруты контроллера
 app.use('/', productRoutes);
 
-// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Сервер працює: http://localhost:${PORT}`);
 });
